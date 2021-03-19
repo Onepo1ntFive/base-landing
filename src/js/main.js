71fde51c5ffe2a23e287ed3c3e9342bea22db89d
Sergey Solovyev <html@laserwar.com>
2021-03-19 17:22:26 +0300

    Commit

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function () {
+
+    //top menu scroll to
+    const menuLinks = document.querySelectorAll('.js-head-menu a');
+
+    Array.prototype.forEach.call(menuLinks, function (el, i) {
+        el.addEventListener('click', () => {
+            // event.preventDefault();
+            let menuTarget = el.getAttribute('href');
+            let menuTargetBlock = document.querySelector(menuTarget);
+
+            scrollIt(menuTargetBlock, 2000);
+        });
+    });
+
+})();
